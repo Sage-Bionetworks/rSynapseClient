abe0c2adea96f0258b6860a4238a641bcc634c50
MattNapsAlot <matt.furia@sagebase.org>
2012-02-05 23:22:46 -0800

    fixed verbose messaging

--- R/checkCurlResponse.R
+++ R/checkCurlResponse.R
@@ -11,9 +11,9 @@
 #			if(info$responsecode != 0 & (info$response.code < 200 || info$response.code >= 300)){
 #				message <- paste("HTTP Error:", info$response.code, "for request", info$effective.url)
 #				if(!missing(response)){
-#					stop(paste(message, response, sep = '\n'))
+#					stop(paste(message, response, sep = '\n'), call.=FALSE)
 #				} else{
-#					stop(message)
+#					stop(message, call.=FALSE)
 #				}
 #			}
 #		}
@@ -28,10 +28,10 @@
 			if(grepl('The AWS Access Key Id you provided does not exist in our records.', response)) {
 				# The propagation delay for new IAM users is anywhere from 5 to 60 seconds
 				stop(paste("Try your request again, but if it doesn't work within 2 minutes, contact the Synapse team for help.", 
-								message, response, sep = '\n'))
+								message, response, sep = '\n'), call.=FALSE)
 			}
 			else {
-				stop(paste(message, response, sep = '\n'))
+				stop(paste(message, response, sep = '\n'), call.=FALSE)
 			}
 		} else{
 			stop(message)
