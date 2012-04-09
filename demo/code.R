d80e518efeb06742a28876d0e9e97daaa50087da
MattNapsAlot <matt.furia@sagebase.org>
2012-04-09 00:12:20 +0000

    Implemented Readable/Writable FileOwner and ObjectOwner classes.
    git-svn-id: https://sagebionetworks.jira.com/svn/PLFM/branches/mfuria/rClientRefactor/client/rSynapseClient@7079 e6edf6fb-f266-4316-afb4-e53d95876a76

--- demo/code.R
+++
@@ -1,4 +0,0 @@
-myHeatmap <- function(x){
-  myName <- "<your name>"
-  heatmap(x, main=sprintf("%s's Custom Heatmap", myName))  
-}
--- inst/unitTests/test_now.R
+++
@@ -1,9 +0,0 @@
-## Unit tests now function
-## 
-## Author: Nicole Deflaxu <nicole.deflaux@sagebase.org>
-###############################################################################
-
-unitTestNow <- function() {
-	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\d \\d\\d:\\d\\d:\\d\\d.*", synapseClient:::.now()))
-	checkTrue(grepl("\\d\\d\\d\\d-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\dZ", synapseClient:::.nowAsString()))
-}
