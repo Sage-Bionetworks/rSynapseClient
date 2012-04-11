884dc42990080612a43c2e8d05e6c7c7f798fddf
brucehoff <bruce.hoff@sagebase.org>
2012-04-11 11:37:03 -0700

    initial commit of entityToFileCache

---
+++ R/synapseRepoServiceEndpoint.R
@@ -0,0 +1,31 @@
+## get/set the Synapse repo service endpoint
+## 
+## Author: Matthew D. Furia <matt.furia@sagebase.org>
+###############################################################################
+
+synapseRepoServiceEndpoint <- 
+  function(endpoint)
+{
+  if (!missing(endpoint)) {
+    .setCache("reposerviceEndpoint", endpoint)
+    url <- .ParsedUrl(url=endpoint)
+    .setCache("reposerviceEndpointLocation", paste(url@protocol, '://', url@authority, sep=''))
+    .setCache("reposerviceEndpointPrefix", url@path)
+    if(!is.null(.getCache('useJavaClient')) &&  .getCache('useJavaClient')){
+      .jenv[["syn"]]$setRepositoryEndpoint(endpoint)
+    }
+    synapseClient:::.setCache("sessionToken", NULL)
+    synapseClient:::.setCache("hmacSecretKey", NULL) 
+  }
+  else {
+    return(.getCache("reposerviceEndpoint"))
+  }
+}
+
+.getRepoEndpointLocation <- function() {
+  .getCache("reposerviceEndpointLocation")	
+}
+
+.getRepoEndpointPrefix <- function() {
+  .getCache("reposerviceEndpointPrefix")	
+}
