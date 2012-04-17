14369e5b38177c0832edc19d26a152bbf665e1c8
Matthew D. Furia <matt.furia@sagebase.org>
2012-04-17 12:12:46 -0700

    finished porting over integration tests and from old code base

---
+++ inst/integrationTests/test_storeDownloadEntityFiles.R
@@ -0,0 +1,53 @@
+# Test functiosn for downloading entity files
+# 
+# Author: Matthew D. Furia <matt.furia@sagebase.org>
+###############################################################################
+
+.setUp <-
+  function()
+{
+  ## create a project
+  project <- new(Class="Project")
+  propertyValues(project) <- list(
+    name = paste("myProject", gsub(':', '_', date()))
+  )
+  project <- createEntity(project)
+  synapseClient:::.setCache("testProject", project)
+  
+  ## create a study
+  study <- Study(list(name="MyDataSet", parentId=propertyValue(project, "id")))
+  study <- createEntity(study)
+  synapseClient:::.setCache("testStudy", study)
+}
+
+.tearDown <-
+  function()
+{
+  deleteEntity(synapseClient:::.getCache("testProject"))
+  synapseClient:::.deleteCache("testProject")
+  synapseClient:::.deleteCache("testStudy")
+}
+
+integrationTestStoreNoFiles <-
+  function()
+{
+  study <- synapseClient:::.getCache("testStudy")
+  data <- Data(list(name='blah',type="C", parentId=propertyValue(study,'id')))
+  data <- storeEntity(data)
+  checkTrue(!is.null(data))
+  checkTrue(!is.null(propertyValue(data,"id")))
+}
+
+integrationTestDownloadNoFiles <-
+  function()
+{
+  study <- synapseClient:::.getCache("testStudy")
+  data <- Data(list(name='blah',type="C", parentId=propertyValue(study,'id')))
+  data <- storeEntity(data)
+  checkTrue(!is.null(data))
+  checkTrue(!is.null(propertyValue(data,'id')))	
+  data <- downloadEntity(propertyValue(data,"id"))        
+  
+  data <- loadEntity(propertyValue(data,"id"))
+}
+
