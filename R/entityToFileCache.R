9e144f3d66684bdeca55ab441619160040b2a312
brucehoff <bruce.hoff@sagebase.org>
2012-04-21 18:15:50 -0700

    SYNR-140

--- R/entityToFileCache.R
+++ R/entityToFileCache.R
@@ -35,7 +35,8 @@ createSynapseEntity<- function(synapseEntity) {
 getEntityFromSynapse <- function(id, version=NULL)
 {
 	entityCachePath <- .entityFileCachePath(id, version)
-	.downloadToFile(.getEntityUri(id, version), entityCachePath, .getCache("ENTITY_FILE_NAME"))
+	content <- .downloadToFile(.getEntityUri(id, version), entityCachePath, .getCache("ENTITY_FILE_NAME"))
+	SynapseEntity(content)
 }
 
 # This function downloads the annotations of a Synapse entity into the local file cache:
@@ -45,7 +46,8 @@ getEntityFromSynapse <- function(id, version=NULL)
 getAnnotationsFromSynapse <- function(id, version=NULL)
 {
 	entityCachePath <- .entityFileCachePath(id, version)
-	.downloadToFile(.getAnnotationsUri(id, version), entityCachePath, .getCache("ANNOTATIONS_FILE_NAME"))
+	content <- .downloadToFile(.getAnnotationsUri(id, version), entityCachePath, .getCache("ANNOTATIONS_FILE_NAME"))
+	SynapseAnnotations(content)
 }
 
 #
@@ -75,6 +77,7 @@ getAnnotationsFromFileCache<- function(id, version=NULL) {
 	content <- .synapseGetDelete(uri, "GET")
 	if (is.null(content)) stop(paste("No result for GET", uri))
 	.writeToFile(gsub("[\r\n]", "", toJSON(content)), filePath, fileName)
+	content
 }
 
 # note 'filePath' omits the cache root, which is prepended by this function
@@ -142,7 +145,7 @@ updateEntityInSynapse<-function(id, version=NULL) {
 	filePath <- paste(.getAbsoluteFileCachePath(.entityFileCachePath(id, version)), 
       .getCache("ENTITY_FILE_NAME"), sep="/")
 	content <- as.list(fromJSON(readLines(filePath), simplifyWithNames=FALSE))
-	.synapsePostPut(uri, content, "PUT")
+	getEntityInstance(.synapsePostPut(uri, content, "PUT"))
 }
 
 deleteEntityFromSynapse<-function(id, version=NULL) {
@@ -183,8 +186,8 @@ updateAnnotationsInSynapse<-function(id, version=NULL) {
     }
   }
   
-	.synapsePostPut(.getAnnotationsUri(id, version), 
-			annotations, "PUT")
+  SynapseAnnotations(.synapsePostPut(.getAnnotationsUri(id, version), 
+			annotations, "PUT"))
 }
 
 
@@ -192,44 +195,6 @@ deleteAnnotationsFromFileCache<-function(id, version=NULL) {
 	file <- paste(.getAbsoluteFileCachePath(.entityFileCachePath(id, version)), 
       .getCache("ANNOTATIONS_FILE_NAME"), sep="/")
 	if (file.exists(file)) unlink(file)
-}
-
-# NOTE:  This method is not used
-# 
-# localSynapseEntity -- a copy of the SynapseEntity with local modifications
-# remoteSynapseEntity -- a recently retrieved copy of the SynapseEntity
-# returns the merged entity, i.e. a copy of the remoteEntity (especially having the
-# remote entity's e-tag) with the local entity's data superimposed)
-#
-# Note:  In this initial implementation no sophisticated merging is attempted
-# Rather the local values simply replace the remote values
-#
-# NOTE:  This method is not used
-#
-mergeSynapseEntities<- function(localSynapseEntity, remoteSynapseEntity) {
-	if (is.null(propertyValue(remoteSynapseEntity, "id"))) stop("entity id is required")
-	
-	# first 'clone' the remote entity
-	result<-new("SynapseEntity")
-	for (n in propertyNames(remoteSynapseEntity)) {
-		propertyValue(result, n)<-propertyValue(remoteSynapseEntity, n)
-	}
-	for (n in annotationNames(remoteSynapseEntity)) {
-		annotValue(result, n)<-annotValue(remoteSynapseEntity, n)
-	}
-	result@synapseEntityKind <- remoteSynapseEntity@synapseEntityKind
-	result@synapseWebUrl<-remoteSynapseEntity@synapseWebUrl
-	
-	# now copy the local properties (except id and etag)
-	for (n in propertyNames(localSynapseEntity)) {
-		if (n=="id" || n=="etag") continue
-		propertyValue(result, n)<-propertyValue(localSynapseEntity, n)
-	}
-	for (n in annotationNames(localSynapseEntity)) {
-		annotValue(result, n)<-annotValue(localSynapseEntity, n)
-	}
-	
-	result
 }
 
 # takes a new SynapseEntity, not yet persisted, and sends it to Synapse 
@@ -273,42 +238,72 @@ getSynapseEntity<- function(id) {
 
 # updates the content of the given SynapseEntity in the local file
 # cache and in Synapse.   Returns the updated entity
+#
+# The steps for updating an entity and its annotations are as follows:
+# 1. We start with a local copy of, which has an etag
+# 2. Push the Entity part (the properties) to Synapse.
+#	 This will either return an updated entity with a new ('intermediate') 
+#	 etag or throw a ConcurrentUpdateException.
+#	 If no error we know that NEITHER the entity NOR its annotations have
+#	 been updated since we originally fetched them.  Now:  
+# 3. Put the new etag in the local copy of the annotations and update in Synapse
+#    This will either return an exception or a new annotations object with new etag
+# 4. Combine the properties returned in 2, the annotations and etag returned in 3,
+#	 into a new object to return from this method.
+#    Note:  When the 'dust settles', if successful, the returned object
+#	 will have had its etag incremented by _2_.
+#
+#
 updateSynapseEntity<-function(synapseEntity) {
 	id<-propertyValue(synapseEntity, "id")
 	if (is.null(id)) stop ("entity id is required")
 
 	updateEntityInFileCache(synapseEntity)
-	updateEntityInSynapse(id)
+	newEntity <- updateEntityInSynapse(id) # note, this omits annotations and has the 'intermediate' etag
 	
-	# now download the annotations to the local cache
-	getAnnotationsFromSynapse(id)
-	# read annotations from local cache into memory
-	annots <- getAnnotationsFromFileCache(id)
-    
-    # merge annotations from input variable into 'annots'
-    # TODO this logic *merges* only.  Need to allow local copy to *delete* annotations
-    for (n in annotationNames(synapseEntity)) {
-      annotValue(annots, n)<-annotValue(synapseEntity, n)
-    }
-    
-    ## make sure all scalars are converted to lists since the service expects all 
-    ## annotation values to be arrays instead of scalars
-    for(key in names(annots)){
-      ## This is one of our annotation buckets
-      if(is.list(annots[[key]])) {
-        for(annotKey in names(annots[[key]])) {
-          if(is.scalar(annotations[[key]][[annotKey]])) {
-            annots[[key]][[annotKey]] <- list(annots[[key]][[annotKey]])
-          }
-        }
-      }
-    }
-  
-	# now persist to file cache and to Synapse
+	annots <- synapseEntity@annotations
+	propertyValue(annots, "etag")<-propertyValue(newEntity, "etag")
+	propertyValue(annots, "id")<-propertyValue(newEntity, "id")
 	updateAnnotationsInFileCache(annots)
-	updateAnnotationsInSynapse(id)
+	newAnnots <- updateAnnotationsInSynapse(id)
+	propertyValue(newAnnots, "id")<-propertyValue(newEntity, "id")
+	updateAnnotationsInFileCache(newAnnots)
 	
-	getSynapseEntity(id)
+	propertyValue(newEntity, "etag") <- propertyValue(newAnnots, "etag")
+	newEntity@annotations <- newAnnots
+	updateEntityInFileCache(newEntity)
+	
+	newEntity
+		
+#	# now download the annotations to the local cache
+#	getAnnotationsFromSynapse(id)
+#	# read annotations from local cache into memory
+#	annots <- getAnnotationsFromFileCache(id)
+#    
+#    # merge annotations from input variable into 'annots'
+#    # TODO this logic *merges* only.  Need to allow local copy to *delete* annotations
+#    for (n in annotationNames(synapseEntity)) {
+#      annotValue(annots, n)<-annotValue(synapseEntity, n)
+#    }
+#    
+#    ## make sure all scalars are converted to lists since the service expects all 
+#    ## annotation values to be arrays instead of scalars
+#    for(key in names(annots)){
+#      ## This is one of our annotation buckets
+#      if(is.list(annots[[key]])) {
+#        for(annotKey in names(annots[[key]])) {
+#          if(is.scalar(annotations[[key]][[annotKey]])) {
+#            annots[[key]][[annotKey]] <- list(annots[[key]][[annotKey]])
+#          }
+#        }
+#      }
+#    }
+#  
+#	# now persist to file cache and to Synapse
+#	updateAnnotationsInFileCache(annots)
+#	updateAnnotationsInSynapse(id)
+#	
+#	getSynapseEntity(id)
 }
 
 # deletes the given SynapseEntity from the local cache and from Synapse
