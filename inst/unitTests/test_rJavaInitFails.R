0fc6ff45dbf8944845c555bf590625298980233a
Matthew D. Furia <matt.furia@sagebase.org>
2012-08-18 15:42:18 -0700

    fixed test_rJavaInitFails.R to warn rather than fail when rJava is not installed

--- inst/unitTests/test_rJavaInitFails.R
+++ inst/unitTests/test_rJavaInitFails.R
@@ -1,23 +1,27 @@
-.setUp <-
-  function()
-{
-	myJinit <- function(classpath){ stop("failed to initialize") }
-	attr(myJinit, "oldDef") <- rJava::.jinit
-	assignInNamespace(".jinit", myJinit, "rJava")
-}
+if("rJava" %in% installed.packages()){
+	.setUp <-
+	  function()
+	{
+		myJinit <- function(classpath){ stop("failed to initialize") }
+		attr(myJinit, "oldDef") <- rJava::.jinit
+		assignInNamespace(".jinit", myJinit, "rJava")
+	}
 
-.tearDown <-
-  function()
-{
-	assignInNamespace(".jinit", attr(rJava::.jinit, "oldDef"), "rJava")
-	unloadNamespace('synapseClient')
-	library(synapseClient)
-}
+	.tearDown <-
+	  function()
+	{
+		assignInNamespace(".jinit", attr(rJava::.jinit, "oldDef"), "rJava")
+		unloadNamespace('synapseClient')
+		library(synapseClient)
+	}
 
-unitTestInitFails <-
-  function()
-{
-	unloadNamespace('synapseClient')
-	library(synapseClient, quiet=TRUE)
-	checkEquals(synapseClient:::.getCache('useJava'), FALSE)
+	unitTestInitFails <-
+	  function()
+	{
+		unloadNamespace('synapseClient')
+		library(synapseClient, quiet=TRUE)
+		checkEquals(synapseClient:::.getCache('useJava'), FALSE)
+	}
+}else{
+	warning("rJava is not installed. skipping tests in tst_rJavaInitFails.R")
 }
