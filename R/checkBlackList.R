926d7b274739b2fadc6b993b6e9e95a984f4b917
brucehoff <bruce.hoff@sagebase.org>
2012-12-13 10:35:24 -0800

    SYNR-286

--- R/checkBlackList.R
+++ R/checkBlackList.R
@@ -7,19 +7,69 @@
 checkBlackList<-function() {
   # get my own version
   myOwnVersion<-packageDescription("synapseClient", fields="Version")
-  .checkBlackListGivenMyVersion(myOwnVersion)
+  serverVersion<-getServerVersion()
+  .checkBlackListGivenMyVersion(myOwnVersion, serverVersion)
 }
 
 # provided for integration testing
-.checkBlackListGivenMyVersion<-function(myOwnVersion) {
+.checkBlackListGivenMyVersion<-function(myOwnVersion, serverVersion) {
   if (is.null(myOwnVersion) || myOwnVersion=="") return
   
   # get the latest version, release notes, black list, and optional user message
   versionInfo <- fromJSON(getURL(.getVersionsEndpoint()))
   
-  if (any(versionInfo$blacklist==myOwnVersion)) {
+  if (.versionIsBlackListed(myOwnVersion, serverVersion, versionInfo$blackList)) {
+    # TODO:  what if myOwnVerion==versionInfo$latestVersion ??
     stop(sprintf("This version of the Synapse Client, %s, has been disabled.  Please upgrade to the latest version, %s.\n%s",
         myOwnVersion, versionInfo$latestVersion, versionInfo$message))
   }
 }
+
+.versionIsBlackListed<-function(clientVersion, serverVersion, blackList) {
+  for (entry in blackList) {
+    if (entry$server=="*" || entry$server==serverVersion) {
+      if (.versionMeetsConstraint(clientVersion, entry$client)) {
+        return(TRUE)
+      }
+  }
+  FALSE
+}
+
+# given a version and a version constraint (e.g. "0.10-1", "<1.5", ">=0.09")
+# does the version meet the constraint?
+# Note: use compareVersion from utils package to do the comparison
+.versionMeetsConstraint<-function(version, constraint) {
+  if (substring(constraint, 1, 1)=="<") {
+    if (substring(constraint, 1, 2)=="<=") {
+      # <=
+      constraintVersion <- substring(constraint, 3)
+      comparison <- compareVersion(version, constraintVersion)
+      comparison==-1 || comparison==0
+    } else {
+      # <
+      constraintVersion <- substring(constraint, 2)
+      comparison <- compareVersion(version, constraintVersion)
+      comparison==-1
+    }
+  } else if (substring(constraint, 1, 1)==">") {
+    if (substring(constraint, 1, 2)==">=") {
+      # >=
+      constraintVersion <- substring(constraint, 3)
+      comparison <- compareVersion(version, constraintVersion)
+      comparison==1 || comparison==0
+    } else {
+      # >
+      constraintVersion <- substring(constraint, 2)
+      comparison <- compareVersion(version, constraintVersion)
+      comparison==1
+    }
+  } else {
+    # == (implicitly)
+    constraintVersion <- constraint
+    comparison <- compareVersion(version, constraintVersion)
+    comparison==0
+  }
+}
+
+
 
