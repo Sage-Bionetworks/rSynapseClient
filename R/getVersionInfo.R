6abda1c3a7b91cc9948cdedc535054e9e8ea2841
brucehoff <bruce.hoff@sagebase.org>
2013-01-23 14:37:32 -0800

    local cache for versions/blacklist info

---
+++ R/getVersionInfo.R
@@ -0,0 +1,21 @@
+#
+# getVersionInfo
+# retrieves the black list / latest version info for the R client, using a cache to avoid repeated web calls
+#
+#
+
+getVersionInfo<-function() {
+  cacheTimestampName<-"versionsInfoTimestamp"
+  cacheVersionInfoName<-"versionsInfo"
+  cacheRefreshSeconds<-300 # 5 minutes
+  now<-Sys.time()
+  cacheTimestamp<-.getCache(cacheTimestampName)
+  versionInfo<-.getCache(cacheVersionInfoName)
+  if (is.null(versionInfo) || is.null(cacheTimestamp) || Sys.time()-now>cacheRefreshSeconds) {
+    response<-getURLWithRetries(.getVersionsEndpoint(), opts=.getCache("curlOpts"))$response
+    versionInfo <- fromJSON(response$body)
+    .setCache(cacheTimestampName, now)
+    .setCache(cacheVersionInfoName, versionInfo)
+  }
+  versionInfo
+}
