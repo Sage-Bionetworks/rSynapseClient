3cb0c52864ff74fadd0fb906ddd184d4e16765d0
Joseph Wu <kaysoky88@gmail.com>
2013-07-30 12:20:54 -0700

    Changed Config from a refClass to an S4 class.  Changed parsing code to use nested lists and vectors.

--- R/Config.R
+++ R/Config.R
@@ -1,44 +1,43 @@
 
-setRefClass("Config",
-  fields = list(config = "matrix", lastSection = "character"),
-  methods = list(
-    initialize = function(configFile){
-      if (!file.exists(configFile)) {
-        return()
-      }
-      configFile <- readLines(configFile)
-      
-      # Takes a string and splits it by the first "="
-      # If the split is successful, returns a vector of the line's [key, value, section]
-      sectionize <- function(input){
-        # Note: strsplit does not split API keys correctly, since multiple "=" may be adjacent to each other
-        inputChars <- unlist(strsplit(input, ""))
-        canSplit <- grep("=", inputChars, fixed=TRUE)
-        if (length(canSplit)) {
-          canSplit <- canSplit[1]
-          itemKey <- do.call(paste, as.list(c(head(inputChars, n=canSplit-1), sep="")))
-          itemVal <- do.call(paste, as.list(c(tail(inputChars, n=length(inputChars)-canSplit), sep="")))
-          return(c(itemKey, itemVal, lastSection))
+setClass("Config", 
+    representation(data = "list", filepath = "character"))
+    
+ConfigParser <- function(path) {
+    config <- new("Config")
+    if (!file.exists(path)) {
+        stop(sprintf("Configuration file %s cannot be found.", path))
+    }
+
+    config@filepath <- path
+    configFile <- readLines(path)
+    section = NULL
+    for (line in configFile) {
+        # Sections
+        matches <- regexec("\\[(.+)\\]", line)
+        matches <- unlist(regmatches(line, matches))
+        if (length(matches) > 1) {
+            section <- matches[2]
+            config@data[[section]] <- c()
+            next
         }
         
-        if (regexpr("\\[.*\\]", input) >= 0) lastSection <<- input
-        return()
-      }
-      config <<- do.call("rbind", lapply(configFile, sectionize))
-      
-      # Trim whitespace
-      config <<- apply(config, c(1, 2), function(vec){ gsub("(^ +)|( +$)", "", vec) })
-    },
-    
-    has.option = function(section, option){
-      return(length(get.section(section, option)) > 0)
-    }, 
-    
-    get.option = function(section, option){
-      section <- paste("[", section, "]", sep="")
-      section <- grep(section, config, fixed=TRUE)
-      option <- grep(option, config[section - 2 * dim(config)[1]], fixed=TRUE)
-      return(config[section[option] - dim(config)[1]])
+        # Options and values
+        matches <- regexec("\\s*([^=]+)\\s*=\\s*([^=]+)\\s*", line)
+        matches <- unlist(regmatches(line, matches))
+        if (length(matches) > 2) {
+            
+            matches <- sapply(matches, function(vec){ gsub("(^ +)|( +$)", "", vec) })
+            config@data[[section]][[matches[2]]] <- matches[3]
+        }
     }
-  )
-)
+    
+    return(config)
+}
+
+Config.hasOption <- function(config, section, option) {
+    return(option %in% names(config@data[[section]]))
+}
+
+Config.getOption <- function(config, section, option) {
+    return(config@data[[section]][[option]])
+}
