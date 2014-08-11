4ec0a0003346c2ce2ecd996902d1fa65c9e6b33d
brucehoff <bruce.hoff@sagebase.org>
2014-08-11 16:23:27 -0700

    SYNR-694

--- R/xxxTableSchema.R
+++ R/xxxTableSchema.R
@@ -5,16 +5,6 @@
 
 
 
-setClass(
-  Class = "TableSchema",
-  contains = "Entity",
-  # This is modeled after defineEntityClass in AAAschema
-  prototype = prototype(
-    synapseEntityKind = "TableSchema",
-    properties = initializeProperties("org.sagebionetworks.repo.model.table.TableEntity", TRUE)
-  )
-)
-
 # creates a TableSchema from the JSON response body received from Synapse
 # this is referenced by 'getEntityInstance' which dispatches based on
 # concreteType
@@ -34,6 +24,7 @@ setMethod(
 
 TableSchema<-function(name, parent, columns, ...) {
   result<-TableSchema()
+  result@properties <- initializeProperties("org.sagebionetworks.repo.model.table.TableEntity", TRUE)
   propertyValue(result, "name")<-name
   if (is(parent, "Entity")) {
     parentId<-propertyValue(parent, "id")
