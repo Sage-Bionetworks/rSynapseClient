5720db7ab28a69482fef61e6f6139b2207e5f82b
brucehoff <bruce.hoff@sagebase.org>
2014-09-06 20:23:41 -0700

    SYNR-694

--- inst/unitTests/test_table.R
+++ inst/unitTests/test_table.R
@@ -3,7 +3,7 @@
 # Author: brucehoff
 ###############################################################################
 
-test_parseRowAndVersion-function() {
+test_parseRowAndVersion<-function() {
   checkEquals(synapseClient:::parseRowAndVersion(c("1-2", "2-3", "3-2")), rbind(c(1,2,3), c(2,3,2)))
   checkException(synapseClient:::parseRowAndVersion(c("1-2", "2-3", "3")))
   checkException(synapseClient:::parseRowAndVersion(c("1-2", "2-3", "3-x")))
