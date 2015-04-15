c396a30cf1b8f1d42ac2a92845a2f12a9c79857e
bhoff <bruce.hoff@sagebase.org>
2015-04-15 11:02:17 -0700

    SYNR-869

--- R/xxxTableSchema.R
+++ R/xxxTableSchema.R
@@ -19,8 +19,9 @@ setMethod(
 )
 
 getTableSchemaColumns<-function(tableId) {
+	if (is.null(tableId)) stop("tableId is required")
 	response<-synRestGET(sprintf("/entity/%s/column", tableId))
-	createS4ObjectFromList(response$results, "TableColumnList")
+	createTypedListFromList(response$results, "TableColumnList")
 }
 
 # this is called from synGet to do TableSchema-specific work
@@ -77,25 +78,30 @@ setMethod(
 		definition = function(entity, activity=NULL, used=NULL, executed=NULL, activityName=NULL, activityDescription=NULL, createOrUpdate=T, forceVersion=T, isRestricted=F, contentType=NULL) {
 			# first, store any unsaved columns
 			idsFromColumns<-list()
-			columnsToStore<-TableColumnList()
+			columnsToStoreAsList<-list()
 			for (column in entity@columns@content) {
 				if (length(column$id)==0) {
-					columnsToStore<-append(columnsToStore, column)
+					columnsToStoreAsList<-append(columnsToStoreAsList, list(createListFromS4Object(column)))
 				} else {
 					idsFromColumns<-append(idsFromColumns, column$id)
 				}
 			}
-			response<-synRestPOST("/column/batch", list(list=createListFromS4Object(columnsToStore)))
-			storedColumns<-createS4ObjectFromList(response$list, "TableColumnList")
-			for (column in storedColumns@content) {
-				idsFromColumns<-append(idsFromColumns, column@id)
+			if (length(columnsToStoreAsList)>0) {
+				response<-synRestPOST("/column/batch", 
+						list(list=columnsToStoreAsList, concreteType="org.sagebionetworks.repo.model.table.ColumnModel"))
+				storedColumns<-createTypedListFromList(response$list, "TableColumnList")
+				for (column in storedColumns@content) {
+					idsFromColumns<-append(idsFromColumns, column@id)
+				}
 			}
 			# now make sure the entity's ID list includes those of the columns
 			propertyValue(entity, "columnIds")<-unique(append(propertyValue(entity, "columnIds"), idsFromColumns))
 			# now do the standard operations for storing an Entity
 			storedEntity<-synStoreMethod(entity, activity, used, executed, activityName, activityDescription, createOrUpdate, forceVersion, isRestricted, contentType)
 			# finally, get a fresh copy of the columns
-			storedEntity@columns<-getTableSchemaColumns(propertyValue(tableSchema, "id"))
+			storedEntity@columns<-getTableSchemaColumns(propertyValue(storedEntity, "id"))
+			
+			storedEntity
 		}
 )
 
