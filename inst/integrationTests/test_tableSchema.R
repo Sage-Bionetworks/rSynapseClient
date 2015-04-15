e0bf03ae87d04fb1df2fd7bb75abb6df8c5c6954
bhoff <bruce.hoff@sagebase.org>
2015-04-15 11:37:57 -0700

    SYNR-869

--- inst/integrationTests/test_tableSchema.R
+++ inst/integrationTests/test_tableSchema.R
@@ -24,17 +24,22 @@ integrationTestCreateTableSchema<-function() {
     tableColumn<-TableColumn(
       name=sprintf("R_Client_Integration_Test_Column_Name_%d", i), 
       columnType="STRING")
-    stored<-synStore(tableColumn)
-    tableColumns<-append(tableColumns, stored)
+	
+	  # it should work whether or not you store the column first
+    if (i<3) tableColumn<-synStore(tableColumn)
+		if (i==2) {
+			# it should work if you just provide the ID
+			tableColumns<-append(tableColumns, tableColumn@id)
+		} else {
+			tableColumns<-append(tableColumns, tableColumn)
+		}
   }
+	# this point tableColumns is a list of (1) a stored column, (2) an unstored column, (3) a column ID
   
   name<-sprintf("R_Client_Integration_Test_Create_Schema_%s", sample(999999999, 1))
  
   tableSchema<-TableSchema(name=name, parent=propertyValue(project, "id"), columns=tableColumns,  foo="bar", "pi"=3.14)
-  for (i in 1:3) {
-		checkEquals(tableColumns[[i]], tableSchema@columns[[i]])
-	}
-  
+	
   storedSchema<-synStore(tableSchema)
   id<-propertyValue(storedSchema, "id")
   checkTrue(!is.null(id))
@@ -53,7 +58,22 @@ integrationTestCreateTableSchema<-function() {
   
   checkEquals(synGetAnnotation(retrievedSchema, "foo"), "bar")
   checkEquals(synGetAnnotation(retrievedSchema, "pi"), 3.14)
-  
+	
+	# check that that retrieved columns match the retrieved column IDs
+	checkEquals(3, length(retrievedSchema@columns))
+	checkEquals(3, length(propertyValue(retrievedSchema, "columnIds")))
+	for (i in 1:3) {
+		checkTrue(any(retrievedSchema@columns[[i]]$id==propertyValue(retrievedSchema, "columnIds")))
+	}
+	
+	# now check that the retrieved table columns match the originals
+	# since we didn't save #3 ahead of time, we just copy over the id here
+	tableColumns[[3]]@id<-retrievedSchema@columns[[3]]@id
+	columnIdToColumnMap<-list()
+	for (i in 1:3) {
+		columnIdToColumnMap[[retrievedSchema@columns[[i]]@id]]<-retrievedSchema@columns[[i]]
+	}
+	
   synDelete(storedSchema)
   
   # check synDelete
