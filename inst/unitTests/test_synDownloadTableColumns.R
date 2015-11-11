3da78066066885365f236fe3716d65df0ded6571
bhoff <bruce.hoff@sagebase.org>
2015-11-11 15:03:10 -0800

    SYNR-950

--- inst/unitTests/test_synDownloadTableColumns.R
+++ inst/unitTests/test_synDownloadTableColumns.R
@@ -37,6 +37,7 @@ unitTest_synDownloadTableColumnsHappyPath<-function() {
 	}
 	df<-data.frame(string=c("a", "b", "c"), files=fileHandleIds, stringsAsFactors=FALSE)
 	table<-Table("syn123", df)
+	expectedDownloadResult<-c()
 	synapseClient:::.mock("downloadTableFileHandles",
 			function(fhasToDownload) {
 				successes<-list()
@@ -44,6 +45,7 @@ unitTest_synDownloadTableColumnsHappyPath<-function() {
 				for (fha in fhasToDownload) {
 					fhId<-fha@fileHandleId
 					filePath<- tempfile(tmpdir=synapseClient:::defaultDownloadLocation(fhId))
+					expectedDownloadResult<-c(expectedDownloadResult, filePath)
 					connection<-file(filePath)
 					writeChar("this is a test", connection, eos=NULL)
 					close(connection)  
@@ -55,8 +57,49 @@ unitTest_synDownloadTableColumnsHappyPath<-function() {
 				list() # return the permanent failures (NONE)
 			}
 	)
-	result<-synDownloadTableColumns(table, "files")
-	# TODO assertions for result
+	downloadResult<-synDownloadTableColumns(table, "files")
 	
-	# TODO add non-happy cases (permanent failures, temporary failures)
+	names(expectedDownloadResult)<-fileHandleIds
+	checkEquals(downloadResult, expectedDownloadResult)
 }
+
+unitTest_synDownloadTableColumnsIllegalFile<-function() {
+	fileHandleIds<-as.character(sample(1000000, 3))
+	illegalFHID<-fileHandleIds[length(fileHandleIds)] # let's say the last one is illegal (invalid or forbidden)
+	# make sure there is no cached information
+	for (fileHandleId in fileHandleIds) {
+		unlink(synapseClient:::defaultDownloadLocation(fileHandleId))
+	}
+	df<-data.frame(string=c("a", "b", "c"), files=fileHandleIds, stringsAsFactors=FALSE)
+	table<-Table("syn123", df)
+	expectedDownloadResult<-c()
+	synapseClient:::.mock("downloadTableFileHandles",
+			function(fhasToDownload) {
+				successes<-list()
+				fhIds<-c()
+				permanentFailures<-list() 
+				for (fha in fhasToDownload) {
+					fhId<-fha@fileHandleId
+					if (fhId==illegalFHID) {
+						permanentFailures[[fhId]]<-"NOT FOUND"
+					} else {
+						filePath<- tempfile(tmpdir=synapseClient:::defaultDownloadLocation(fhId))
+						expectedDownloadResult<-c(expectedDownloadResult, filePath)
+						connection<-file(filePath)
+						writeChar("this is a test", connection, eos=NULL)
+						close(connection)  
+						synapseClient:::addToCacheMap(fhId, filePath)
+						fhIds<-c(fhIds, fhId)
+					}
+				}
+				permanentFailures
+			}
+	)
+	downloadResult<-synDownloadTableColumns(table, "files")
+	
+	checkEquals(2, length(expectedDownloadResult)) # we only get two of three
+	names(expectedDownloadResult)<-fileHandleIds
+	checkEquals(downloadResult, expectedDownloadResult)
+
+}
+
