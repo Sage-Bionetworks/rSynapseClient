29d33d1d9f6aa7ea9733cd948824f0d3525ced09
bhoff <bruce.hoff@sagebase.org>
2017-05-18 15:11:58 -0700

    made entity query tests more robust

--- inst/integrationTests/test_QueryResult.R
+++ inst/integrationTests/test_QueryResult.R
@@ -21,18 +21,37 @@
   deleteEntity(synapseClient:::.getCache("testProject"))
 }
 
+timeToWait<-"00:10:00" # wait for up to ten minutes
+
 integrationTestQueryResult_Fetch <- function() {
   project <- synapseClient:::.getCache("testProject")$properties$id
 
   ## add some children
   folder <- Folder(parentId=project)
   lapply(1:10, function(i) createEntity(folder))
-  Sys.sleep(30)
-  qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=5)
-  df <- qr$fetch()
+
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+  	qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=5)
+  	df <- qr$fetch()
+		if (nrow(df)==5) break
+		message("Expected 5 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df),5)
   checkEquals(ncol(df),3)
-  df <- qr$fetch()
+	
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+		df <- qr$fetch()
+		if (nrow(df)==5) break
+		message("Expected 5 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df),5)
   checkEquals(ncol(df),3)
 
@@ -49,14 +68,41 @@ integrationTestQueryResult_Collect <- function() {
   folder <- Folder(parentId=project)
   lapply(1:10, function(i) createEntity(folder))
 
-  qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=3)
-  df <- qr$collect()
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+		qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s'", project), blockSize=3)
+		df <- qr$collect()
+		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df),3)
   checkEquals(ncol(df),3)
-  df <- qr$collect()
+	
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+		df <- qr$collect()
+		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df),3)
   checkEquals(ncol(df),3)
-  df <- qr$collect()
+	
+	
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+		df <- qr$collect()
+		if (nrow(df)==3) break
+		message("Expected 3 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df),3)
   checkEquals(ncol(df),3)
 
@@ -71,10 +117,19 @@ integrationTestQueryResult_CollectAll <- function() {
   ## add some children
   folder <- Folder(parentId=project)
   lapply(1:10, function(i) createEntity(folder))
-
-  qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s' LIMIT 10", project), blockSize=7)
-  qr$collect()
-  df <- qr$collectAll()
+	
+	
+	startTime<-Sys.time()
+	while (Sys.time()-startTime<as.difftime(timeToWait)) {
+		qr <- synapseClient:::QueryResult$new(sprintf("select id, name, parentId from entity where parentId=='%s' LIMIT 10", project), blockSize=7)
+		qr$collect()
+		df <- qr$collectAll()
+		if (nrow(df)==10) break
+		message("Expected 10 rows but found ", nrow(df))
+		Sys.sleep(5)
+	}
+	message("Waited ", difftime(Sys.time(), startTime, units="mins"), " minutes for query results.")
+	
   checkEquals(nrow(df), 10)
 }
 
