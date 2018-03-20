0e694068a9591e06d8a43bdbf861c4c26f635050
bhoff <bruce.hoff@sagebase.org>
2018-03-20 14:37:17 -0700

    SYNR-1251

--- inst/unitTests/test_FileNameFromHeaders.R
+++ inst/unitTests/test_FileNameFromHeaders.R
@@ -20,4 +20,9 @@ unitTestWrongHeaderName <- function() {
 	checkTrue(is.null(synapseClient:::fileNameFromHeaders(headers)))
 }
 
+unitTestQuotedFileName <- function() {
+	headers<-"x-amz-id-2: +iB0K6cFV3I1eUO7YIGJlF3Wn7GVxLKZEPsO1dbo5DYzwwo0lBVBlCqKV4W8S4w5\r\nx-amz-request-id: A2EA02790F1D642A\r\nDate: Thu, 26 Mar 2015 17:11:54 GMT\r\nContent-Disposition: attachment; filename=\"file7697fb85a1e.rbin\"\r\nLast-Modified: Tue, 13 Jan 2015 02:03:02 GMT\r\n"
+	checkEquals(synapseClient:::fileNameFromHeaders(headers), "file7697fb85a1e.rbin")
+}
+
 
